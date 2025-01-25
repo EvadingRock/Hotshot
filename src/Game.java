cee1953b2cbe776cc30edaab508265943cc82910
Evading Rock <115958501+EvadingRock@users.noreply.github.com>
2025-01-25 18:57:19 -0500

    setup for hsa2 graphics and workspace

---
+++ src/Game.java
@@ -0,0 +1,24 @@
+/*
+ * Game.java
+ * by Caden
+ * 
+ * file started: Saturday, January 25, 2025
+ * 
+ * this is the main file of the game of hotshot
+ * the game will be a 3d game in which you can hook to objects and swing
+ * done with hsa2 graphics
+/*/
+
+import hsa2.GraphicsConsole;
+
+public class Game {
+	public static void main(String[] args) {
+		new Game();
+	}
+
+	GraphicsConsole gc;
+
+	Game() {
+		
+	}
+}
